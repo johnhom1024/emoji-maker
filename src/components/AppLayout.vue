da67cde9fd3cbae6cd04a33ad0c4a4b502f49fc9
anthony <924358746@qq.com>
2022-08-27 10:41:22 +0800

    feat: 新增AppLayout组件，用于主题色

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <view :style="[themeColor]">
+    <slot></slot>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      primaryColor: "blue",
+    };
+  },
+  computed: {
+    themeColor() {
+      const style = {};
+
+      style["--color-primary"] = this.primaryColor;
+
+      return style;
+    },
+  },
+};
+</script>
---
+++ src/pages/demo/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <app-layout ref="applayout">
+    <view class="color-primary"> hello world </view>
+    <button @click="changeColor">改变颜色</button>
+  </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/components/AppLayout.vue';
+export default {
+  components: {
+    AppLayout
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    changeColor() {
+      this.$refs.applayout.primaryColor = 'red';
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.color-primary {
+  color: var(--color-primary);
+}
+</style>
