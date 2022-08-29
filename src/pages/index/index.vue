d251bc43e7930410afaeccbf10d8dbf865d61874
anthony <924358746@qq.com>
2022-08-29 12:02:52 +0800

    feat: 完善页面使用tailwindcss

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,14 +5,14 @@
  * @Description: 
 -->
 <template>
-  <view class="full flex flex-col">
-    <div class="top-bar px-[30rpx] flex">
+  <view class="min-h-screen flex flex-col">
+    <div class="px-30 py-10 flex">
       <div class="w-200">
         <u-button size="small" type="error" shape="circle" @click="clearCanvas"
           >清空画布</u-button
         >
       </div>
-      <div class="spacer"></div>
+      <div class="flex-auto"></div>
       <div class="w-200">
         <u-button size="small" type="success" shape="circle" @click="save"
           >保存相册</u-button
@@ -20,18 +20,17 @@
       </div>
     </div>
     <!-- 画布 -->
-    <view class="vh-50 drawer">
+    <view class="h-[50vh] p-30 bg-[#efefef]">
       <canvas
         type="2d"
         id="canvas"
-        class="canvas-container"
-        style="width: 100%; height: 100%"
+        class="bg-white border-2 rounded-2 border-solid border-[#e1e1e1] w-full h-full"
         :disable-scroll="true"
         @touchstart="handleTouchstart"
         @touchmove="handleTouchmove"
       ></canvas>
     </view>
-    <view class="operation flex-auto flex flex-column">
+    <view class="pt-20 px-30 flex-auto flex flex-col">
       <div class="mb-30">
         <!-- 操作 -->
         <u-subsection
@@ -46,21 +45,23 @@
         <view class="mb-20">
           <u-button type="primary" @click="chooseImage"> 选择图片 </u-button>
         </view>
-        <view class="input-wrapper mb-20 flex align-center">
-          <view class="w-60 mr-20">
+        <view class="mb-20 flex items-center">
+          <view class="flex-auto mr-20">
             <u--input
               placeholder="请输入内容"
               v-model="inputText"
               @input="handleInputText"
             ></u--input>
           </view>
-          <u-button @click="addText">加上文字</u-button>
+          <view  class="w-200">
+            <u-button @click="addText">加上文字</u-button>
+          </view>
         </view>
       </template>
 
       <!-- 素材 -->
       <template v-if="currentIndex === 1">
-        <div class="source-material-list">
+        <div class="-ml-6 -mr-6">
           <u-grid :col="4" :border="false" @click="handleClickMaterial">
             <u-grid-item
               v-for="img in materialList"
@@ -68,11 +69,13 @@
               :name="img"
               :customStyle="gridItemStyle"
             >
-              <div class="source-material-item w-full">
+              <div
+                class=" pt-[100%] relative rounded-8 border-2 border-solid border-[#d9d9d9] overflow w-full overflow-hidden"
+              >
                 <image
                   :src="img"
                   mode="aspectFill"
-                  class="source-material-image"
+                  class="absolute top-0 left-0 w-full h-full"
                 ></image>
               </div>
             </u-grid-item>
@@ -184,110 +187,5 @@ page {
   min-height: 100vh;
   box-sizing: border-box;
   // background-color: #fff;
-}
-</style>
-
-<style lang="scss" scoped>
-.full {
-  min-height: 100vh;
-}
-
-.p-8 {
-  padding: 8rpx;
-}
-
-.w-full {
-  width: 100%;
-}
-.w-200 {
-  width: 200rpx;
-}
-
-.spacer {
-  flex-grow: 1;
-}
-.px-30 {
-  padding: 10rpx 30rpx 10rpx;
-}
-.vh-50 {
-  height: 50vh;
-}
-.mr-20 {
-  margin-right: 20rpx;
-}
-.align-center {
-  align-items: center;
-}
-.w-60 {
-  flex: 0 0 60%;
-}
-.flex {
-  display: flex;
-}
-
-.flex-column {
-  flex-direction: column;
-}
-.flex-auto {
-  flex: auto;
-}
-.w-half {
-  width: 50%;
-  box-sizing: border-box;
-}
-.border-none {
-  border: 2rpx solid #999999;
-  &::after {
-    border: none;
-  }
-}
-.left-button {
-  border-radius: 10rpx 0 0 10rpx;
-  border-right: none;
-}
-.right-button {
-  border-radius: 0 10rpx 10rpx 0;
-  border-left: none;
-}
-.mb-20 {
-  margin-bottom: 20rpx;
-}
-
-.mb-30 {
-  margin-bottom: 30rpx;
-}
-.drawer {
-  padding: 30rpx;
-  background-color: #efefef;
-}
-.canvas-container {
-  background: #fff;
-  border: 2rpx solid #e1e1e1;
-}
-.operation {
-  padding: 20rpx 30rpx 0;
-}
-// .input-wrapper {
-//   padding: ;
-// }
-
-.source-material-list {
-  margin-left: -6rpx;
-  margin-right: -6rpx;
-}
-.source-material-item {
-  width: 100%;
-  padding-top: 100%;
-  position: relative;
-  border: 2rpx solid #d9d9d9;
-  border-radius: 8rpx;
-  overflow: hidden;
-  .source-material-image {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
 }
 </style>
